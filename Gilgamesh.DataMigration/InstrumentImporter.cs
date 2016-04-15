d326808f3dfa8afac95fe38f07c1f2b24a4809db
Julien Husser <thisisjulienh@gmail.com>
2016-04-15 22:33:44 +0200

    Import of Shares

--- Gilgamesh.DataMigration/InstrumentImporter.cs
+++ Gilgamesh.DataMigration/InstrumentImporter.cs
@@ -18,10 +18,10 @@ namespace Gilgamesh.DataMigration
 
         public static void ImportCashInstruments()
         {
-            CashStandardMetaModel cashStandardMetaModel= new CashStandardMetaModel();
+            CashStandardMetaModel cashStandardMetaModel = new CashStandardMetaModel();
             foreach (KeyValuePair<int, string> currency in CurrenciesList.Currencies)
             {
-                CashInstrument cash = new CashInstrument {CurrencyId = currency.Key,MetaModel = cashStandardMetaModel,Name = String.Format("Cash Instrument {0}",currency.Value),Reference = new Reference{Name = String.Format("Cash Instrument {0}", currency.Value) ,ReferecenceTypeId = 1} };
+                CashInstrument cash = new CashInstrument {CurrencyId = currency.Key, MetaModel = cashStandardMetaModel, Name = String.Format("Cash Instrument {0}", currency.Value), Reference = new Reference { Name = String.Format("Cash Instrument {0}", currency.Value), ReferecenceTypeId = 1 } };
                 UnitOfWorkFactory.Instance.UnitOfWork.Instruments.Add(cash);
             }
             UnitOfWorkFactory.Instance.UnitOfWork.Complete();
@@ -31,10 +31,55 @@ namespace Gilgamesh.DataMigration
         {
             ShareStandardMetaModel shareStandardMetaModel = new ShareStandardMetaModel();
 
-            Share share = new Share{Reference = new Reference {Name = "VOO", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "VANGUARD 500 ETF", MetaModel = shareStandardMetaModel,MarketId = 109};
-            UnitOfWorkFactory.Instance.UnitOfWork.Instruments.Add(share);
+            foreach (Share share in Shares)
+            {
+                UnitOfWorkFactory.Instance.UnitOfWork.Instruments.Add(share);
+            }
             UnitOfWorkFactory.Instance.UnitOfWork.Complete();
         }
 
+
+
+        private static readonly List<Share> Shares = new List<Share>
+        {
+            //ETFs
+            new Share{Reference = new Reference {Name = "GLD", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "SPDR Gold Shares", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "VNQ", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "VANGUARD REIT ETF", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "REM", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "iShares Mortgage Real Estate Capped", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "TLO", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "SPDR Barclays Long Term Treasury ETF", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "SHY", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "iShares 1-3 Years treasury Bonds", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "JNK", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "SPDR Barclays High Yield Bond ETF", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "HYG", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "iShares iBoxx high Yield Corporate Bond", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "VOO", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "VANGUARD 500 ETF", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "ACWX", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "iShares MSCI ACWI ex US", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "CIU", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "iShares Intermediate Credit Bond", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+
+
+            //Shares CTO
+            new Share{Reference = new Reference {Name = "APWC", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "Asia Pacific Wire & Cable Ltd", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "DSWL", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "Deswell Industries Inc", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "DPM", ReferecenceTypeId = 1} ,CurrencyId = 5,Name = "Dundee Precious Metals Inc.", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 118},
+            new Share{Reference = new Reference {Name = "MSN", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "Emerson Radio Corp.", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "GSOL", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "Global sources Ltd", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "HYD", ReferecenceTypeId = 1} ,CurrencyId = 5,Name = "Hyduke Energy Services Inc", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 118},
+            new Share{Reference = new Reference {Name = "MUBL", ReferecenceTypeId = 1} ,CurrencyId = 4,Name = "MBL Group PLC", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 160},
+
+            //Shares PEA
+            new Share{Reference = new Reference {Name = "ALP", ReferecenceTypeId = 1} ,CurrencyId = 2,Name = "ADL Partner", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 125},
+            new Share{Reference = new Reference {Name = "DBT", ReferecenceTypeId = 1} ,CurrencyId = 2,Name = "Encres Dubuit", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 125},
+            new Share{Reference = new Reference {Name = "EXAC", ReferecenceTypeId = 1} ,CurrencyId = 2,Name = "Exacompta Clairefontaine", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 125},
+            new Share{Reference = new Reference {Name = "GEA", ReferecenceTypeId = 1} ,CurrencyId = 2,Name = "GEA SA", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 125},
+            new Share{Reference = new Reference {Name = "IRC", ReferecenceTypeId = 1} ,CurrencyId = 2,Name = "Irce Spa", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 140},
+            new Share{Reference = new Reference {Name = "NTZ", ReferecenceTypeId = 1} ,CurrencyId = 1,Name = "Natuzzi Spa-ADR", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 109},
+            new Share{Reference = new Reference {Name = "PHO", ReferecenceTypeId = 1} ,CurrencyId = 4,Name = "Peel Hotels PLC", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 160},
+            new Share{Reference = new Reference {Name = "GIRO", ReferecenceTypeId = 1} ,CurrencyId = 2,Name = "Signaux Girod", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 125},
+            new Share{Reference = new Reference {Name = "VIN", ReferecenceTypeId = 1} ,CurrencyId = 2,Name = "Vianini Industria", MetaModel = MetaModelsList.ShareStandardMetaModel,MarketId = 140}
+        };
+
+    }
+
+    public class MetaModelsList
+    {
+        public static readonly ShareStandardMetaModel ShareStandardMetaModel = new ShareStandardMetaModel();
     }
 }
